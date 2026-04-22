9021cdc801aad52c632f0ba9672207a406a775f7
aaronw176 <aaronw176@nycstudents.net>
2026-04-22 09:24:11 -0400

    api

--- app.py
+++ app.py
@@ -2,28 +2,43 @@ import tkinter as tk
 import requests
 window = tk.Tk()
 window.title("useless facts")
-window.geometry("400x250")
-window.resizable(False, False)
-prompt = tk.Label(window, text="Would you like to know a useless fact", font=("Arial", 16))
+window.geometry("1920x1000")
+window.resizable(True, True)
+
+prompt = tk.Label(window, text="Would you like to know a useless fact", font=("Arial", 24), justify="center")
 prompt.pack(pady=10)
+
 def afact():
     response = requests.get("https://uselessfacts.jsph.pl/api/v2/facts/random?language=en")
     if response.status_code != 200:
         print("Error fetching data!") 
         return None
     data = response.json()
-    print(data['text']) 
-def no_command():
-    print("Ok smart guy I see how it is")
-def nothing():
-    print("Bro what is the point of clicking me I do nothing, stop wasting your time")
+    return data
+ 
+
+def change_nothing_button():
+    prompt.config(text="Bro what is the point of clicking me I do nothing, stop wasting your time")
+
+
+def change_no_button():
+    prompt.config(text=f"{"Ok smart guy I see how it is, GET OUT!"}")
+    window.after(1500, window.destroy)
+def change_button():
+    afact()
+    fact = afact()
+    prompt.config(text=(f"{fact['text']} Another one?"))
+
 fact_button = tk.Button(
-    window, text="Yes", font=("Arial", 14), width=10, command=afact)
-fact_button.place(x=60, y=75)
+    window, text="Yes", font=("Arial", 20), width=20, height=5, command=change_button, relief="groove")
+fact_button.place(x=500, y=400)
+
 no_button = tk.Button(
-    window, text="No", font=("Arial", 14), width=10, command=no_command)
-no_button.place(x=225,y=75)
+    window, text="No", font=("Arial", 20), width=20, height=5,  command=change_no_button, relief="ridge")
+no_button.place(x=1100,y=400)
+
 button = tk.Button(
-    window, text="", font=("Arial", 14), command=nothing)
+    window, text="", command=change_nothing_button)
 button.place(x=195,y=60)
+
 window.mainloop()  
